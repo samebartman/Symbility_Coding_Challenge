f265fb56afa8be49fc15f8a8041450e6c894f3c6
samebartman <47392560+samebartman@users.noreply.github.com>
2019-02-11 17:17:30 -0500

    Update Script_2.py

--- Script_2.py
+++ Script_2.py
@@ -2,20 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import time
-user = "symbilitytestscripts@gmail.com"
-pwd = "abc123--"
+user = "symbilitytestscripts@gmail.com" #username for test amazon account
+pwd = "abc123--" #password for test amazon account
 driver = webdriver.Chrome("/Users/samanthabartman/Desktop/chromedriver")
 
 driver.get("https://www.amazon.ca")
 assert "Amazon" in driver.title
 time.sleep(5)
-elem = driver.find_element_by_xpath('//*[@id="nav-link-yourAccount"]')
-ActionChains(driver).click(elem).perform()
+elem = driver.find_element_by_xpath('//*[@id="nav-link-yourAccount"]') #causes mouse to hover over Sign In CTA
+ActionChains(driver).click(elem).perform() #causes mouse to click on Sign In CTA
 time.sleep(5)
-email = driver.find_element_by_name("email")
-email.send_keys(user)
-password = driver.find_element_by_name("password")
-password.send_keys(pwd)
+email = driver.find_element_by_name("email") #finds the username textbox
+email.send_keys(user) #types username in textbox
+password = driver.find_element_by_name("password") #finds the password textbox
+password.send_keys(pwd) #types password in textbox
 password.send_keys(Keys.RETURN)
 time.sleep(5)
 assert "No results found." not in driver.page_source
